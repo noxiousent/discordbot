e0107b08868d3cad757c651f3f9d5dcda37c52ca
ash <ashrith78912345@gmail.com>
2020-01-16 20:55:01 +0530

    everything , might have to change procfile

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const {token} = require('config.json')
+
+
+client.login(token)
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+
+
+  client.on('message', message => {
+    // If the message is "ping"
+    if (message.content.startsWith('!findthehomo')) {
+      // Send "pong" to the same channel
+      //message.channel.send('pong');
+
+        // giphy.gifByID("ZGC27UvQJxXT37XzAh").then((response)=>{
+        //     console.log(typeof(response));
+            message.channel.send("finding homos....\n"+"one FAG FOUND as per "+message.member.displayName + "'s query",
+            {
+                files : ['https://media.giphy.com/media/ZGC27UvQJxXT37XzAh/giphy.gif']
+            }
+            );
+         }
+         //.catch(()=> {
+        //     message.channel.send("error")
+        // })
+
+    })
+
